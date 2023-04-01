69c8bd75e23e93c32ce20c23cd69472e96b9d3e9
David Johnson <david@struct.com.au>
2023-04-01 19:28:14 +1100

    Adjust colour for contrast

--- tailwind.default.js
+++ tailwind.default.js
@@ -110,7 +110,7 @@ module.exports = {
       loose: 2,
     },
     colors: {
-      primary: "#0B604F",
+      primary: "#107F69",
       secondary: "#F5A623",
       tertiary: "#4A90E2",
       highlight: "#E6E6E6",
