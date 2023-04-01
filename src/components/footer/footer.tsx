addc28a63def2921270de97251ec6d50b37ef2fd
David Johnson <david@struct.com.au>
2023-04-01 18:25:14 +1100

    Fix styles for layout

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -9,10 +9,10 @@ const FooterWrapper = tw.footer`
   flex bg-primary flex-1
 `
 const FooterContainer = tw.div`
-  flex flex-col flex-wrap grow p-4 md:flex-row lg:mx-auto lg:max-w-3/4
+  flex flex-col md:flex-wrap grow p-4 md:flex-row lg:mx-auto lg:max-w-3/4
 `
 const FooterGroup = tw.nav`
-  flex flex-col basis-1/2 mb-4 grow-0 shrink-0 lg:basis-1/4 lg:mb-0
+  flex flex-col shrink grow mb-4 md:grow-0 md:shrink-0 md:basis-1/2 lg:basis-1/4 lg:mb-0
 `
 const FooterSubheading = tw(Heading)`
   text-white font-subheading vr-tight-22 text-left
@@ -35,9 +35,9 @@ export const Footer = () => {
             <FooterGroup>
               <FooterSubheading size="h5">{name}</FooterSubheading>
               {pages?.map(page => {
-                const { full_slug, name, content } = page
+                const { full_slug, name } = page
                 return (
-                  <FooterLink to={full_slug}>{name}</FooterLink>
+                  <FooterLink key={full_slug} to={`/${full_slug}`}>{name}</FooterLink>
                 )
               })}
             </FooterGroup>
