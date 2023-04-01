7c41e71ec5596d539dc2d5debd129cad49614e17
David Johnson <david@struct.com.au>
2023-04-01 18:12:45 +1100

    Add optional chaining operator

--- src/components/page/page.tsx
+++ src/components/page/page.tsx
@@ -3,6 +3,6 @@ import { FC } from 'react'
 import { Blok } from 'storyblok-react'
 
 export const Page: FC<Blok<any>> = ({ story }: Blok<any>) => {
-  const components = story.content.body.map(blok => (<StoryblokComponent blok={blok} key={blok._uid} />))
+  const components = story?.content?.body?.map(blok => (<StoryblokComponent blok={blok} key={blok._uid} />))
   return components
 }
